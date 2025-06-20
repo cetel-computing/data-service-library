7924fdd05c52234ad0b848498a4a120aa2ab1da1
Lindsay <lindsay@cetel-computing.org>
2025-06-20 16:06:45 +0000

    Removed unused refs

--- DataServices/Config/LdapConfigOptions.cs
+++ DataServices/Config/LdapConfigOptions.cs
@@ -12,29 +12,7 @@ namespace DataServices.Config
 
         public string UsersOu { get; set; }
 
-        public string MyNetworksOu { get; set; }
-
         public string OrganisationsOu { get; set; }
-
-        public int OrgDnElements { get; set; }
-
-        public int DomainDnElements { get; set; }
-
-        public string BlacklistedOu { get; set; }
-
-        public string WhitelistedOu { get; set; }
-
-        public string OrgAdminCn { get; set; }
-
-        public string DomainAdminCn { get; set; }
-
-        public string FinanceAdminCn { get; set; }
-
-        public string DashboardUserCn { get; set; }
-
-        public string O365AuthorisedSendersOu { get; set; }
-
-        public string GwsAuthorisedSendersOu { get; set; }
 
         public string MyNetworksCn { get; set; }
     }
